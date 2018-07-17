40213ce78a0a84aacfe9f5082a067c7353a0173d
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-16 21:23:18 -0700

    testing divide operator

--- csharp/rational-numbers/RationalNumbers.cs
+++ csharp/rational-numbers/RationalNumbers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class RealNumberExtension
 {
@@ -11,8 +13,11 @@ public static class RealNumberExtension
 
 public struct RationalNumber
 {
+    public int Numerator, Denominator;
     public RationalNumber(int numerator, int denominator)
     {
+        Numerator = numerator;
+        Denominator = denominator;
     }
 
     public RationalNumber Add(RationalNumber r)
@@ -22,7 +27,10 @@ public struct RationalNumber
 
     public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        var n = r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator;
+        var d = r1.Denominator * r2.Denominator;
+
+        return new RationalNumber(n, d).Reduce();
     }
 
     public RationalNumber Sub(RationalNumber r)
@@ -32,7 +40,7 @@ public struct RationalNumber
 
     public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        return r1 + new RationalNumber(r2.Numerator * -1, r2.Denominator);
     }
 
     public RationalNumber Mul(RationalNumber r)
@@ -42,7 +50,10 @@ public struct RationalNumber
 
     public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        var n = r1.Numerator * r2.Numerator;
+        var d = r1.Denominator * r2.Denominator;
+
+        return new RationalNumber(n, d).Reduce();
     }
 
     public RationalNumber Div(RationalNumber r)
@@ -52,7 +63,7 @@ public struct RationalNumber
 
     public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        return r1 * new RationalNumber(r2.Denominator, r2.Numerator);
     }
 
     public RationalNumber Abs()
@@ -62,7 +73,14 @@ public struct RationalNumber
 
     public RationalNumber Reduce()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(Numerator == 0) Denominator = 1;
+
+        var gcd = GCD(Numerator, Denominator);
+
+        Numerator /= gcd;
+        Denominator /= gcd;
+
+        return this;
     }
 
     public RationalNumber Exprational(int power)
@@ -73,5 +91,33 @@ public struct RationalNumber
     public double Expreal(int baseNumber)
     {
         throw new NotImplementedException("You need to implement this function.");
+    }
+
+    private int GCD(int n1, int n2)
+    {
+        var mults1 = Multiples(Math.Abs(n1));
+        var mults2 = Multiples(Math.Abs(n2));
+
+        var common = mults1.Intersect(mults2);
+
+        return common.Max();
+    }
+
+    private static IEnumerable<int> Multiples(int number)
+    {
+        var mults = new List<int>() {1};
+
+        var c = 2;
+
+        // lazy!  use primes.
+        while(c < number)
+        {
+            if(number % c == 0) mults.Add(c);
+            c++;
+        }
+
+        mults.Add(number);
+
+        return mults;
     }
 }
