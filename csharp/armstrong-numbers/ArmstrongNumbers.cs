9540b7aa5f7726d9dab30ecafb61def21a51a5f7
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-16 19:01:45 -0700

    simplified

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -4,6 +4,12 @@ public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sum = 0;
+        foreach(var digit in number.ToString())
+        {
+            sum += (int)Math.Pow((int)char.GetNumericValue(digit),Math.Floor(Math.Log10(number) + 1));
+        }
+
+        return sum == number;
     }
 }
