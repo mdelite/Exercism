cfe0daba69ed1d22c3d4d201f120346db688cd24
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-21 14:39:14 -0700

    all tests passed

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public class BinarySearch
+{
+    private int[] _searchArray;
+    private int loc = 0;
+
+    public BinarySearch(int[] input)
+    {
+        _searchArray = new int[input.Length];
+        Array.Copy(input, _searchArray, input.Length);  
+    }
+
+    public int Find(int value)
+    {
+        if(_searchArray.Length < 1) return -1;
+
+        var mid = _searchArray.Length / 2;
+        var val = _searchArray[mid];
+      
+        if(val == value)
+        {
+            return mid + loc;
+        }
+        else if(_searchArray.Length > 1 && value < val)
+        {
+            _searchArray = _searchArray.Take(mid).ToArray();
+            return Find(value);
+        } else if(_searchArray.Length > 1 && value > val)
+        {
+            _searchArray = _searchArray.Skip(mid).ToArray();
+            loc += mid;
+            return Find(value);
+        } 
+        return -1;
+    }
+}
