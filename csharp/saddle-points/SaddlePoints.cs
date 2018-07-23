c1bf87720e3d07e825e3f4f16b1663b8b69d4e8d
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-23 16:55:48 -0700

    all tests passed

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -4,12 +4,41 @@ using System.Linq;
 
 public class SaddlePoints
 {
-    public SaddlePoints(int[,] values)
-    {
-    }
+    private int[,] _matrix;
+    public SaddlePoints(int[,] values) => _matrix = values;
 
     public IEnumerable<(int, int)> Calculate()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var saddles = new List<(int, int)>();
+
+        var colMin = new int?[_matrix.GetLength(1)];   
+        for(var r = 0; r < _matrix.GetLength(0); r++)
+        {
+            var row = Enumerable.Range(0, _matrix.GetUpperBound(1) + 1)
+                      .Select(i => _matrix[r, i])
+                      .ToArray();
+
+            var rowMax = row.Max();
+
+            for(var c = 0; c < _matrix.GetLength(1); c++)
+            {
+                if(colMin[c] == null) colMin[c] = GetColumnMin(c);
+
+                var val = _matrix[r, c];
+
+                if(val == rowMax && val == colMin[c]) saddles.Add((r, c));
+            }
+        }
+        
+        return saddles;
+    }
+
+    private int GetColumnMin(int columnIndex)
+    {
+        var col = Enumerable.Range(0, _matrix.GetUpperBound(0) + 1)
+            .Select(i => _matrix[i, columnIndex])
+            .ToArray();
+        
+        return col.Min();
     }
 }
