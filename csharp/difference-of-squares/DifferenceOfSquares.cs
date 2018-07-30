dba6263812722d87573ea28a372ff4f3bbe4a2e5
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-29 19:15:27 -0700

    cleaned up wit LINQ

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,31 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max)
-    {
-        var sum = 0;
-        for(var x = 1; x <= max; x++)
-        {
-            sum += x;
-        }
+    public static int CalculateSquareOfSum(int max) => max
+        .NaturalNumbers()
+        .Sum()
+        .Square();
 
-        return (int)Math.Pow(sum, 2);
-    }
 
-    public static int CalculateSumOfSquares(int max)
-    {
-        var sum = 0;
-        for(var x = 1; x <= max; x++)
-        {
-            sum += (int)Math.Pow(x, 2);
-        }
-
-        return sum;
-    }
+    public static int CalculateSumOfSquares(int max) => max
+        .NaturalNumbers()
+        .Select(Square)
+        .Sum();
 
     public static int CalculateDifferenceOfSquares(int max)
     {
         return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
+
+    private static IEnumerable<int> NaturalNumbers(this int max) => Enumerable.Range(1, max);
+
+    private static int Square(this int num) => num * num;
 }
