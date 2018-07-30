034911a1611f1a038f0bdf8f10a0ed383cfb3daf
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-30 16:22:56 -0700

    submitted

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool IsPangram(string input) => input.ToLowerInvariant()
+            .Where(IsLetter)
+            .Distinct()
+            .Count() == 26;
+
+    public static bool IsLetter(this char c) => (int)c >= 97 && (int)c <= 122;
 }
