baa159400423dc330915916955536947f770045c
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-29 21:34:39 -0700

    simplified

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -5,11 +5,11 @@ public static class Grains
     public static ulong Square(int n)
     {
         if(n < 1 || n > 64) throw new ArgumentOutOfRangeException();
-        return (ulong)Math.Pow(2, n - 1);
+        return 1UL << n - 1;
     }
 
     public static ulong Total()
     {
-        return (ulong)Math.Pow(2, 64) - 1;
+        return ulong.MaxValue;
     }
 }
