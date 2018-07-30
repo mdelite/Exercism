3edd2844956a900c20e57b41163f2e895393c2dd
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-29 18:36:15 -0700

    submitted

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -4,16 +4,28 @@ public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sum = 0;
+        for(var x = 1; x <= max; x++)
+        {
+            sum += x;
+        }
+
+        return (int)Math.Pow(sum, 2);
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sum = 0;
+        for(var x = 1; x <= max; x++)
+        {
+            sum += (int)Math.Pow(x, 2);
+        }
+
+        return sum;
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
 }
