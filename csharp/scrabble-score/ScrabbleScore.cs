0b108e1dd95dedb48416bc941e1786d0903827f7
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-31 19:59:08 -0700

    submitted

---
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ScrabbleScore
+{
+    private static Dictionary<char, int> Scores = new Dictionary<char, int>
+    {
+        {'a', 1},
+        {'b', 3},
+        {'c', 3},
+        {'d', 2},
+        {'e', 1},
+        {'f', 4},
+        {'g', 2},
+        {'h', 4},
+        {'i', 1},
+        {'j', 8},
+        {'k', 5},
+        {'l', 1},
+        {'m', 3},
+        {'n', 1},
+        {'o', 1},
+        {'p', 3},
+        {'q', 10},
+        {'r', 1},
+        {'s', 1},
+        {'t', 1},
+        {'u', 1},
+        {'v', 4},
+        {'w', 4},
+        {'x', 8},
+        {'y', 4},
+        {'z', 10}
+    };
+
+    public static int Score(string input) => input.Select(x => Scores[char.ToLower(x)]).Sum();
+}
