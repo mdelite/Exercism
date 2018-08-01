7294cb72b7234786bf6914324fa61da0d10e75e6
Michael Doolittle <mgdoolittle@gmail.com>
2018-07-31 17:40:57 -0700

    utilized yield

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -5,25 +5,17 @@ public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        var keepers = new List<T>();
-
         foreach(var c in collection)
         {
-            if(predicate(c)) keepers.Add(c);
+            if(predicate(c)) yield return c;
         }
-
-        return keepers;
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        var keepers = new List<T>();
-
         foreach(var c in collection)
         {
-            if(!predicate(c)) keepers.Add(c);
+            if(!predicate(c)) yield return c;
         }
-
-        return keepers;
     }
 }
