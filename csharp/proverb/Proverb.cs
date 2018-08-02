fc0c962aa4fd8b8d1a34642b6921ca5d631475dd
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-01 20:16:16 -0700

    submitted

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Proverb
 {
     public static string[] Recite(string[] subjects)
     {
-        throw new NotImplementedException();
+        var proverb = new List<string>();
+
+        for(var i = 0; i < subjects.Length - 1; i++)
+        {
+            proverb.Add($"For want of a {subjects[i]} the {subjects[i + 1]} was lost.");
+        }
+
+        if(subjects.Length > 0) proverb.Add($"And all for the want of a {subjects[0]}.");
+
+        return proverb.ToArray();
     }
 }
