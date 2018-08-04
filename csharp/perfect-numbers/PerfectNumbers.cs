bb4c3d7144006f19caa0cd03ef54047d651d479f
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-04 09:20:48 -0700

    Submitted
    changed the range from n-1 to n/2 to cut back on loops as there will not
    be a denominator greater than half.

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -10,9 +10,13 @@ public enum Classification
 
 public static class PerfectNumbers
 {
-    public static Classification Classify(int number) => (Classification)Enumerable
-        .Range(1, number - 1)
-        .Where(x => number % x == 0)
-        .Sum()
-        .CompareTo(number);
+    public static Classification Classify(int number)
+    {
+        if(number < 1) throw new ArgumentOutOfRangeException();
+
+        return (Classification)Enumerable
+            .Range(1, number / 2)
+            .Where(x => number % x == 0)
+            .Sum().CompareTo(number);
+    }
 }
