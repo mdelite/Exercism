b97cf5aee75ecbf914fffc8863c6ea8f3f83cdfa
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-04 07:47:06 -0700

    Submitted
    First submittal, all tests passed.

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        var sum = Enumerable.Range(1, number - 1).Where(x => number % x == 0).Sum();
+
+        if(sum > number)
+        {
+            return Classification.Abundant;
+        }
+        else if(sum < number)
+        {
+            return Classification.Deficient;
+        }
+        return Classification.Perfect;
+    }
+}
