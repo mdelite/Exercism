e167fd0c05f47cfc8e4edfe6e416d5743e8cce15
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-03 21:46:52 -0700

    Submitted
    blindly poked through this one.  Is this correct? I don't know.

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        var result = 0;
+
+        if(int.TryParse(input, out result))
+        {
+            return result;
+        }
+        else return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        return int.TryParse(input, out result);
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        disposableObject.Dispose();
+        throw new Exception();
+    }
+}
