64d85c7eab378e7e8f2d906ed89d22273e4a4828
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-04 08:23:56 -0700

    Cleaned Up
    Added values to the enum, changed the main method over to an expression
    body.

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -3,25 +3,16 @@ using System.Linq;
 
 public enum Classification
 {
-    Perfect,
-    Abundant,
-    Deficient
+    Perfect = 0,
+    Abundant = 1,
+    Deficient = -1
 }
 
 public static class PerfectNumbers
 {
-    public static Classification Classify(int number)
-    {
-        var sum = Enumerable.Range(1, number - 1).Where(x => number % x == 0).Sum();
-
-        if(sum > number)
-        {
-            return Classification.Abundant;
-        }
-        else if(sum < number)
-        {
-            return Classification.Deficient;
-        }
-        return Classification.Perfect;
-    }
+    public static Classification Classify(int number) => (Classification)Enumerable
+        .Range(1, number - 1)
+        .Where(x => number % x == 0)
+        .Sum()
+        .CompareTo(number);
 }
