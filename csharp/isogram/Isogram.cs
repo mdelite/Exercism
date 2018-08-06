848bfe12f368ac10add2d74a90f486b267bae9cd
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-05 20:49:09 -0700

    Submitted
    I think this Linq stuff is starting to click with me.

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) => word.Where(char.IsLetter).Select(char.ToLowerInvariant).Distinct().Count() == word.Where(char.IsLetter).Count();
+}
