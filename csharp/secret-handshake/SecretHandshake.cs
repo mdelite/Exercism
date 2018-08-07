e4cc8f48090f822c4e8c2dda91d6215212124464
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-06 17:58:59 -0700

    Second Iteration
    Went more Linqy.

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -5,38 +5,28 @@ using System.Collections.Generic;
 
 public static class SecretHandshake
 {
+    [Flags]
+    private enum Handshakes
+    {
+        none = 0,
+        wink = 1,
+        double_blink = 2,
+        close_your_eyes = 4,
+        jump = 8,
+        reverse = 16
+    }
+    
     public static string[] Commands(int commandValue)
     {
-        var actions = new List<string>();
+        var commands = (Handshakes)commandValue;
+
+        var actions = Enum.GetValues(typeof (Handshakes))
+            .Cast<Handshakes>()
+            .Where(x => commands.HasFlag(x) && x != Handshakes.reverse && x != Handshakes.none )
+            .Select(x => x.ToString().Replace('_', ' '));
         
-        var i = 1;
-        while(i < 16)
-        {
-            if((commandValue & i) > 0)
-            {
-                actions.Add(GetAction(i));  
-            }
-            i = i << 1;
-        }
+        if(commands.HasFlag(Handshakes.reverse)) return actions.Reverse().ToArray();
 
-        if((commandValue & 16) > 0) actions.Reverse();
         return actions.ToArray();
-    }
-
-    private static string GetAction(int i)
-    {
-        switch(i)
-        {
-            case 1:
-                return "wink";
-            case 2:
-                return "double blink";
-            case 4:
-                return "close your eyes";
-            case 8:
-                return "jump";
-            default:
-                throw new ArgumentOutOfRangeException($"No action for '{i}' is defined.");
-        }
     }
 }
