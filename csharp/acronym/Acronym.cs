e5dd6421de6be44158eab6660b2ba74afadc2c72
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-08 18:05:13 -0700

    Submitted
    Pretty happy with this.

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var firstLetter = phrase
+            .Split( new[] {' ', '-'} )
+            .Select(x => Char.ToUpperInvariant(x[0]))
+            .ToArray();
+            
+        return new string(firstLetter);
+    }
+}
