6f7393ef256ac5bc39568a4d499aa72a3c159937
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-11 15:44:54 -0700

    Improved with Local Function
    Discovered 'local functions'.

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -5,20 +5,15 @@ public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        return new string((text.Select(x => Rotate(x, shiftKey)).ToArray()));
-    }
-
-    private static char Rotate(char c, int shiftKey)
-    {
-        if(!char.IsLetter(c)) return c;
-        
-        var shifted = c + shiftKey;
-        
-        while((char.IsLower(c) && shifted > 'z') || (char.IsUpper(c) && shifted > 'Z'))
+        char Rotate(char c)
         {
-            shifted -= 26;
+            if(!char.IsLetter(c)) return c;
+            
+            var firstLetter = char.IsUpper(c) ? 'A' : 'a';
+
+            return (char)(firstLetter + (c - firstLetter + shiftKey) % 26);
         }
 
-        return (char)shifted;
+        return new string((text.Select(Rotate).ToArray()));
     }
 }
