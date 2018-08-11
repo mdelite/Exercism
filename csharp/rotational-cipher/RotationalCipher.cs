93ee417a462acdec871a51938a4ce5b79efba232
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-11 09:59:07 -0700

    Submitted

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Linq;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new string((text.Select(x => Rotate(x, shiftKey)).ToArray()));
+    }
+
+    private static char Rotate(char c, int shiftKey)
+    {
+        if(!char.IsLetter(c)) return c;
+        
+        var shifted = c + shiftKey;
+        
+        while((char.IsLower(c) && shifted > 'z') || (char.IsUpper(c) && shifted > 'Z'))
+        {
+            shifted -= 26;
+        }
+
+        return (char)shifted;
     }
 }
