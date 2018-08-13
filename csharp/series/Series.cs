3db6d294dd9da55d8f3ad122cdbffda7bd1f20be
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-12 19:12:09 -0700

    sUBMItted

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if( sliceLength > numbers.Length || sliceLength <= 0 || numbers.Length == 0)
+            throw new ArgumentException();
+
+        var chunks = new List<string>();
+        for(var i = 0; i <= numbers.Length - sliceLength; i++)
+        {
+            chunks.Add(numbers.Substring(i, sliceLength));
+        }
+        return chunks.ToArray();
     }
 }
