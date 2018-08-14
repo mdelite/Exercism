140f6f9292d95f45e691878d6c50dca5119c2fcf
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-13 19:14:50 -0700

    Final

--- csharp/twelve-days/TwelveDays.cs
+++ csharp/twelve-days/TwelveDays.cs
@@ -22,42 +22,19 @@ public static class TwelveDays
 
     private static string[] days = new[]
     {
-        "first",
-        "second",
-        "third",
-        "fourth",
-        "fifth",
-        "sixth",
-        "seventh",
-        "eighth",
-        "ninth",
-        "tenth",
-        "eleventh",
-        "twelfth"
+        "first", "second", "third", "fourth", "fifth", "sixth",
+        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
     };
-    
-    public static string Recite(int verseNumber)
-    {
-        return $"On the {days[verseNumber - 1]} day of Christmas my true love gave to me, {TheseGifts(verseNumber)}.";
-    }
 
-    private static string TheseGifts(int verseNumber)
-    {
-        if(verseNumber == 1) return gifts[0];
+    public static string Recite(int verseNumber) =>
+        $"On the {days[verseNumber - 1]} day of Christmas my true love gave to me, {TheseGifts(verseNumber)}.";
 
-        var theseGifts = Enumerable.Range(0, verseNumber)
+    private static string TheseGifts(int verseNumber) => Enumerable.Range(0, verseNumber)
             .Reverse()
-            .Select(x => gifts[x])
-            .SkipLast(1)
+            .Select(x => ((x == 0 && verseNumber > 1) ? "and " : "") + gifts[x])
             .Aggregate((a, b) => $"{a}, {b}");
 
-        return $"{theseGifts}, and {gifts[0]}";
-    }
-
-    public static string Recite(int startVerse, int endVerse)
-    {
-        return Enumerable.Range(startVerse, endVerse - startVerse + 1)
+    public static string Recite(int startVerse, int endVerse) => Enumerable.Range(startVerse, endVerse - startVerse + 1)
             .Select(verse => Recite(verse))
             .Aggregate((a, b) => $"{a}\n{b}");
-    }
 }
