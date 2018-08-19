afea17393a755d98b0c108e2914cb461f6ff93e8
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-19 08:50:28 -0700

    Changed to lean on Foldl
    done with help from blu-sta's solution -
    https://exercism.io/tracks/csharp/exercises/list-ops/solutions/8d9d703a57f24884869a144275475030

--- csharp/list-ops/ListOps.cs
+++ csharp/list-ops/ListOps.cs
@@ -1,52 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public static class ListOps
 {
-    public static int Length<T>(List<T> input)
-    {
-        var c = 0;
-        foreach(var item in input)
-        {
-            c++;
-        }
-        return c;
-    }
+    public static int Length<T>(List<T> input) => 
+        Foldl(input, 0, (count, item) => count + 1);
 
-    public static List<T> Reverse<T>(List<T> input)
-    {
-        var output = new List<T>();
-        var start = Length(input) - 1;
-        for (int i = start; i >= 0; i--)
-        {
-            output.Add(input[i]);
-        }
-        return output;
-    }
+    public static List<T> Reverse<T>(List<T> input) => Foldl(input, new List<T>(), (list, item) => 
+        Append(new List<T>() { item }, list));
 
-    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
-    {
-        var output = new List<TOut>();
-        foreach(var item in input)
-        {
-            output.Add(map(item));
-        }
-        return output;
-    }
+    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map) => 
+        Foldl(input, new List<TOut>(), (list, item) => Append(list, new List<TOut>() { map(item) }));
 
-    public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate)
-    {
-        var output = new List<T>();
-        foreach (var item in input)
-        {
-            if(predicate(item))
-            {
-                output.Add(item);
-            }
-        }
-        return output;
-    }
+    public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate) => 
+        Foldl(input, new List<T>(), (list, item) => Append(list, predicate(item) ? new List<T>() { item } : new List<T>()));
 
     public static TOut Foldl<TIn, TOut>(List<TIn> input, TOut start, Func<TOut, TIn, TOut> func)
     {
@@ -58,34 +25,12 @@ public static class ListOps
         return output;
     }
 
-    public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func)
-    {
-        var output = start;
-        var inputReversed = Reverse(input);
-        foreach (var item in inputReversed)
-        {
-            output = func(item, output);
-        }
-        return output;
-    }
+    public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func) => 
+        Foldl(Reverse(input), start, (y, x) => func(x, y));
 
-    public static List<T> Concat<T>(List<List<T>> input)
-    {
-        var output = new List<T>();
-        foreach(var item in input)
-        {
-            output = Append(output, item);
-        }
-        return output;
-    }
+    public static List<T> Concat<T>(List<List<T>> input) => 
+        Foldl(input, new List<T>(), (list, item) => Append(list, item));
 
-    public static List<T> Append<T>(List<T> left, List<T> right)
-    {
-        var output = left;
-        foreach(var item in right)
-        {
-            output.Add(item);
-        }
-        return output;
-    }
+    public static List<T> Append<T>(List<T> left, List<T> right) => 
+        Foldl(right, new List<T>(left), (list, item) => { list.Add(item); return list; });
 }
