21d53b35d31177fe8e5599d7037d308852ed4043
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-20 20:49:56 -0700

    Submitted

--- csharp/simple-cipher/SimpleCipher.cs
+++ csharp/simple-cipher/SimpleCipher.cs
@@ -1,49 +1,69 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class SimpleCipher
 {
+    int[] _key;
+
     public SimpleCipher()
     {
         var rand = new Random();
-        Key = new string (Enumerable.Range(1, 100)
-            .Select(x => (char)('a' + rand.Next(26)))
-            .ToArray());
+        _key = Enumerable.Range(1, 100)
+            .Select(x => rand.Next(26))
+            .ToArray();
     }
 
     public SimpleCipher(string key)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(key == "" || !key.All(char.IsLower)) throw new ArgumentException($"{nameof(key)} must be one or more lowercase letters");
+        _key = key
+            .Select(x => x - 'a')
+            .ToArray();
     }
     
     public string Key 
     {
-        get; private set;
+        get
+        {
+            return new string(_key.Select(x => (char)(x + 'a')).ToArray());
+        }
     }
 
-    public string Encode(string plaintext)
-    {
-        return new string (plaintext.ToLowerInvariant()
-            .Zip(Key, (x, y) => ShiftChar(x, y))
+    public string Encode(string plaintext) => new string(plaintext
+            .Zip(_key.Stretch(plaintext.Length), (x, y) => ShiftChar(x, y))
             .ToArray());
-    }
 
-    public string Decode(string ciphertext)
-    {
-        return new string(ciphertext
-            .Zip(Key, (x, y) => DecodeChar(x, y))
+    public string Decode(string ciphertext) => new string(ciphertext
+            .Zip(_key.Stretch(ciphertext.Length), (x, y) => ShiftChar(x, -y))
             .ToArray());
+
+    private char ShiftChar(char c, int shift)
+    {
+        var n = c + shift;
+        if(n > 'z')
+        {
+            n -= 26;
+        } 
+        else if(n < 'a')
+        {
+            n += 26;
+        }
+        return (char)n;
+    }
+}
+
+static class Extensions
+{
+        public static IEnumerable<T> Stretch<T>(this IEnumerable<T> key, int length)
+    {
+        var newKey = key.ToList();
+
+        while(newKey.Count() < length)
+        {
+            newKey.AddRange(key);
+        }
+        return newKey;
     }
 
-    private char ShiftChar(char c, char key)
-    {
-        var n = c + key - 'a';
-        return n > 'z' ? (char)(n - 26) : (char)n;
-    }
-
-    private char DecodeChar(char c, char key)
-    {
-        var n = c - key + 'a';
-        return n < 'a' ? (char)(n + 26) : (char)n;
-    }
 }
