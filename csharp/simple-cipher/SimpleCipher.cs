2c86c22e7513cbb943b93949a724ef46820f7270
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-21 20:49:07 -0700

    Submitted

--- csharp/simple-cipher/SimpleCipher.cs
+++ csharp/simple-cipher/SimpleCipher.cs
@@ -4,66 +4,42 @@ using System.Collections.Generic;
 
 public class SimpleCipher
 {
-    int[] _key;
-
     public SimpleCipher()
     {
         var rand = new Random();
-        _key = Enumerable.Range(1, 100)
-            .Select(x => rand.Next(26))
-            .ToArray();
+        Key = string.Concat(Enumerable.Range(1, 100)
+            .Select(x => (char)rand.Next('a', 'z' + 1)));
     }
 
     public SimpleCipher(string key)
     {
         if(key == "" || !key.All(char.IsLower)) throw new ArgumentException($"{nameof(key)} must be one or more lowercase letters");
-        _key = key
-            .Select(x => x - 'a')
-            .ToArray();
+        Key = key;
     }
     
     public string Key 
     {
-        get
-        {
-            return new string(_key.Select(x => (char)(x + 'a')).ToArray());
-        }
+        get; private set;
     }
 
-    public string Encode(string plaintext) => new string(plaintext
-            .Zip(_key.Stretch(plaintext.Length), (x, y) => ShiftChar(x, y))
-            .ToArray());
-
-    public string Decode(string ciphertext) => new string(ciphertext
-            .Zip(_key.Stretch(ciphertext.Length), (x, y) => ShiftChar(x, -y))
-            .ToArray());
-
-    private char ShiftChar(char c, int shift)
+    public string Encode(string plaintext) 
     {
-        var n = c + shift;
-        if(n > 'z')
-        {
-            n -= 26;
-        } 
-        else if(n < 'a')
-        {
-            n += 26;
-        }
-        return (char)n;
-    }
-}
+        return Shift(plaintext,(x, y) => (x + y));
+    } 
 
-static class Extensions
-{
-        public static IEnumerable<T> Stretch<T>(this IEnumerable<T> key, int length)
+    public string Decode(string ciphertext)
     {
-        var newKey = key.ToList();
-
-        while(newKey.Count() < length)
-        {
-            newKey.AddRange(key);
-        }
-        return newKey;
+        return Shift(ciphertext, (x, y) => (x - y));
     }
 
+    int CanonicalModulo(int number, int divisor)
+    {
+        return ((number % divisor) + divisor) % divisor;
+    }
+
+    string Shift(string text, Func<int, int, int> func)
+    {
+        var key = string.Concat(Enumerable.Repeat(Key, (int)Math.Ceiling((double)text.Length / Key.Length)));
+        return string.Concat(text.Zip(key, (a, b) => (char)('a' + CanonicalModulo(func(a - 'a', b - 'a'), 26))));
+    }
 }
