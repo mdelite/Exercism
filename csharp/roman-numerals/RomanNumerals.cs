288fadea89b87eef4be2bbf4d9b220f934ac3deb
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-22 18:58:51 -0700

    Better, More Efficient

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -4,30 +4,33 @@ using System.Linq;
 
 public static class RomanNumeralExtension
 {
-    private enum Romans
+    private static Dictionary<int, string> romans = new Dictionary<int, string>()
     {
-        I = 1,
-        IV = 4,
-        V = 5,
-        IX = 9,
-        X = 10,
-        XL = 40,
-        L = 50,
-        XC = 90,
-        C = 100,
-        CD = 400,
-        D = 500,
-        CM = 900,
-        M = 1000        
-    }
+        { 1000, "M"},
+        { 900, "CM"},
+        { 500, "D"},
+        { 400, "CD"},
+        { 100, "C"},
+        { 90, "XC"},
+        { 50, "L"},
+        { 40, "XL"},
+        { 10, "X"},
+        { 9, "IX"},
+        { 5, "V"},
+        { 4, "IV"},
+        { 1, "I"},
+    };
+
     public static string ToRoman(this int value)
     {
         var roman = "";
-        while(value > 0)
+        foreach(var r in romans)
         {
-            var max = Enum.GetValues(typeof(Romans)).Cast<int>().TakeWhile(x => x <= value).Max();
-            roman += ((Romans)max).ToString();
-            value -= max;
+            while(value >= r.Key)
+            {
+                value -= r.Key;
+                roman += r.Value;
+            }
         }
         return roman;
     }
