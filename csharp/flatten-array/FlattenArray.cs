c9dbf72c5b0342601b97ed36dfec063117d73996
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-23 20:14:25 -0700

    Done With Ling

--- csharp/flatten-array/FlattenArray.cs
+++ csharp/flatten-array/FlattenArray.cs
@@ -4,27 +4,9 @@ using System.Linq;
 
 public static class FlattenArray
 {
-    public static IEnumerable Flatten(IEnumerable input)
-    {
-        return input.Cast<object>()
-            .Where(x => x != null)
-            .Select(x => x is IEnumerable ? Flatten(x as IEnumerable) : new object[] { x })
-            .SelectMany(s => s.Cast<object>());
-
-        // foreach(var item in input)
-        // {
-        //     var collection = item as IEnumerable;
-        //     if(collection != null)
-        //     {
-        //         foreach(var colItem in Flatten(collection))
-        //         {
-        //             yield return colItem;
-        //         }
-        //     } 
-        //     else if(item != null)
-        //     {
-        //         yield return item;
-        //     } ;
-        // }
-    }
+    public static IEnumerable Flatten(IEnumerable input) => input
+        .Cast<object>()
+        .Where(x => x != null)
+        .Select(y => y is IEnumerable ? Flatten(y as IEnumerable) : new object[] { y })
+        .SelectMany(z => z.Cast<object>());
 }
