898767bd2bbbca61e59a6c71a44f65553ec8f7bb
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-26 09:07:34 -0700

    All Tests Passed

--- csharp/minesweeper/Minesweeper.cs
+++ csharp/minesweeper/Minesweeper.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Minesweeper
 {
+    private static readonly char mine = '*';
     public static string[] Annotate(string[] input)
     {
-        var field = input;
-        return field;
+        var minefield = new List<string>();
+        for(var row = 0; row < input.Length; row++)
+        {
+            var minefieldRow = "";
+            for(var col = 0; col < input[row].Length; col++)
+            {
+                minefieldRow += input[row][col] == mine ? mine : MineCount(row, col, ref input);
+            }
+            minefield.Add(minefieldRow);
+        }
+        return minefield.ToArray();
+    }
+
+    private static char MineCount(int row, int col, ref string[] input)
+    {
+        var startRow = row == 0 ? 0 : row - 1;
+        var startCol = col == 0 ? 0 : col - 1;
+        var endRow = row + 1 < input.Length ? row + 1 : row;
+        var endCol = col + 1 < input[row].Length ? col + 1 : col;
+
+        var count = 0;
+        for(var r = startRow; r <= endRow; r++)
+        {
+            for(var c = startCol; c <= endCol; c++)
+            {
+                if(input[r][c] == mine) count++;
+            }
+        }
+        return count == 0 ? ' ' : char.Parse(count.ToString());
     }
 }
