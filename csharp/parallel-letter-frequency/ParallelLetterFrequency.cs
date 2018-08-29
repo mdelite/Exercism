89544559ae44b8eea7e2de367ca7ccf1ca1ae7c7
Michael Doolittle <mgdoolittle@gmail.com>
2018-08-28 18:57:30 -0700

    All Tests Pass

---
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+    {
+        return texts.Aggregate("", (x, y) => x + y)
+            .ToLowerInvariant()
+            .Where(x => char.IsLetter(x))
+            .GroupBy(x => x)
+            .ToDictionary(g => g.Key, g => g.Count());  
+    }
+}
