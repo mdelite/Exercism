c066edfb1985e63a505b147fac7277c2b440a37c
Michael Doolittle <mgdoolittle@gmail.com>
2018-09-01 11:50:37 -0700

    First Iteration, All Tests Passed

---
+++ csharp/grep/Grep.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+public static class Grep
+{
+    public static string Match(string pattern, string flags, string[] files)
+    {
+        var sb = new StringBuilder();
+        var caseInsensitive = flags.Contains("-i");
+        var fileMatch = flags.Contains("-l");
+        var lineMatch = flags.Contains("-x");
+        var tagNumber = flags.Contains("-n");
+        var inverted = flags.Contains("-v");
+
+        var testPattern = caseInsensitive ? pattern.ToLowerInvariant() : pattern;
+
+        foreach(var file in files)
+        {
+            var reader = new StreamReader(file);
+            string line;
+            var lineNumber = 0;
+
+            while((line = reader.ReadLine()) != null)
+            {
+                lineNumber++;
+
+                var tag = tagNumber ? $"{lineNumber}:" : "";
+                if(LineMatch(line))
+                {
+                    if(fileMatch)
+                    {
+                        sb.Append($"{(sb.Length > 0 ? "\n" : "")}{file}");
+                        reader.ReadToEnd();
+                    }
+                    else
+                    {
+                        sb.Append($"{(sb.Length > 0 ? "\n" : "")}{(files.Length > 1 ? $"{file}:" : "")}{tag}{line}");
+                    }
+                }
+            }
+            reader.Close();
+        }
+
+        return sb.ToString();
+
+        bool LineMatch(string text)
+        {   
+            var testText = caseInsensitive ? text.ToLowerInvariant() : text;
+            var isMatch = lineMatch ? testText == testPattern : testText.Contains(testPattern);
+            return inverted ? !isMatch : isMatch;
+        }
+    }
+
+}
