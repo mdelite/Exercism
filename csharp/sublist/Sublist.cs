e2450e4da2928b19cde96ebfc03b1a203c69d3f6
Michael Doolittle <mgdoolittle@gmail.com>
2018-09-03 08:26:41 -0700

    Final

--- csharp/sublist/Sublist.cs
+++ csharp/sublist/Sublist.cs
@@ -15,30 +15,16 @@ public static class Sublist
     public static SublistType Classify<T>(List<T> list1, List<T> list2)
         where T : IComparable
     {
-        if(list1.Count == list2.Count)
-            return list1.IsEqual(list2) ? SublistType.Equal : SublistType.Unequal;
+        if(list1.Count < list2.Count)
+            return list2.Contains(list1) ? SublistType.Sublist : SublistType.Unequal;
 
         if(list1.Count > list2.Count)
-            return IsSublist(list2, list1) ? SublistType.Superlist : SublistType.Unequal;
+            return list1.Contains(list2) ? SublistType.Superlist : SublistType.Unequal;
 
-        return IsSublist(list1, list2) ? SublistType.Sublist : SublistType.Unequal;
+        return list1.SequenceEqual(list2) ? SublistType.Equal : SublistType.Unequal;
     }
 
-    private static bool IsSublist<T>(List<T> list1, List<T> list2) where T : IComparable
-    {
-        if (list1.Count == 0) return true;
-
-        return Enumerable.Range(0, list2.Count - list1.Count + 1)
-                    .Select(i => list2.Skip(i).Take(list1.Count).IsEqual(list1))
-                    .Count(x => x) > 0;
-    }
-
-    private static bool IsEqual<T> (this IEnumerable<T> list1, IEnumerable<T> list2 )
-    {
-        if(list1.Count() != list2.Count()) return false;
-        if(list1.Count() == 0) return true;
-
-        return list1.Zip(list2, (a, b) => a.Equals(b))
-            .Aggregate((a, b) => a && b);
-    }
+    private static bool Contains<T>(this List<T> list2, List<T> list1 ) => 
+        Enumerable.Range(0, list2.Count - list1.Count + 1)
+            .Any(i => list2.GetRange(i,list1.Count).SequenceEqual(list1));
 }
