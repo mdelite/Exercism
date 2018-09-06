f51308d3c7cbcd0cee560a39b9f1447e4fb1d329
Michael Doolittle <mgdoolittle@gmail.com>
2018-09-05 20:02:21 -0700

    I Guess...

--- csharp/tree-building/TreeBuilding.cs
+++ csharp/tree-building/TreeBuilding.cs
@@ -8,60 +8,54 @@ public class TreeBuildingRecord
     public int RecordId { get; set; }
 }
 
-public class Tree
+public class Tree : IComparable
 {
-    public int Id { get; set; }
-    public int ParentId { get; set; }
+    public Tree(TreeBuildingRecord r)
+    {
+        if(r.RecordId == 0 && r.ParentId != 0) throw new ArgumentException($"{nameof(r.RecordId)} 0 must have {nameof(r.ParentId)} of 0.");
+        if(r.RecordId != 0 && r.ParentId >= r.RecordId) throw new ArgumentException($"{nameof(r.ParentId)}:'{r.ParentId}' must be less than {nameof(r.RecordId)}:'{r.RecordId}'");
 
-    public List<Tree> Children { get; set; }
+        Id = r.RecordId;
+        ParentId = r.ParentId;
 
-    public bool IsLeaf => Children.Count == 0;
+        Children = new List<Tree>();
+    }
+
+    public int Id { get; }
+    public int ParentId { get; }
+    public List<Tree> Children { get; }
+
+    public bool IsLeaf => Children.Count() == 0;
+
+    public int CompareTo(object obj)
+    {
+        var t = obj as Tree;
+
+        return this.Id - t.Id;
+    }
+
+    internal void AddBranches(IEnumerable<Tree> branches)
+    {
+        var trees = branches.Where(r => r.ParentId == this.Id)
+            .ToList();
+
+        trees.ForEach(t => t.AddBranches(branches.Where(r => r.Id > t.Id)));
+        Children.AddRange(trees);
+        Children.Sort();
+    }
 }
 
 public static class TreeBuilder
 {
     public static Tree BuildTree(IEnumerable<TreeBuildingRecord> records)
     {
-        var ordered = new SortedList<int, TreeBuildingRecord>();
+        var c = records.Count();
+        if(c == 0 || c - 1 != records.Select(rec => rec.RecordId).Max()) throw new ArgumentException();
 
-        foreach (var record in records)
-        {
-            ordered.Add(record.RecordId, record);
-        }
+        var tree = new Tree(records.First(r => r.RecordId == 0));
+        var branches = records.Where(r => r.RecordId > 0).Select(x => new Tree(x));
+        tree.AddBranches(branches);
 
-        records = ordered.Values;
-
-        var trees = new List<Tree>();
-        var previousRecordId = -1;
-
-        foreach (var record in records)
-        {   
-            var t = new Tree { Children = new List<Tree>(), Id = record.RecordId, ParentId = record.ParentId };
-            trees.Add(t);
-
-            if ((t.Id == 0 && t.ParentId != 0) ||
-                (t.Id != 0 && t.ParentId >= t.Id) ||
-                (t.Id != 0 && t.Id != previousRecordId + 1))
-            {
-                throw new ArgumentException();
-            }
-
-            ++previousRecordId;
-        }
-        
-        if (trees.Count == 0)
-        {
-            throw new ArgumentException();
-        }
-
-        for (int i = 1; i < trees.Count; i++)
-        {
-            var t = trees.First(x => x.Id == i);
-            var parent = trees.First(x => x.Id == t.ParentId);
-            parent.Children.Add(t);
-        }
-
-        var r = trees.First(t => t.Id == 0);
-        return r;
+        return tree;
     }
 }
