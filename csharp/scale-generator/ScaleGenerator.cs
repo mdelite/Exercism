a51bd25f5bce407cb05be1a24ff3f7f1abd6b7a0
Michael Doolittle <mgdoolittle@gmail.com>
2018-09-08 14:26:46 -0700

    Purely Cosmetic...

--- csharp/scale-generator/ScaleGenerator.cs
+++ csharp/scale-generator/ScaleGenerator.cs
@@ -7,52 +7,20 @@ public static class ScaleGenerator
     private static string[] notes = new[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"  };
     private static string[] notesFlat = new[] { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
     private static string[] flatKeys = new[] { "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"};
-    public static string[] Pitches(string tonic)
+    private static Dictionary<char, int> step = new Dictionary<char, int>() {{'m', 1}, {'M', 2}, {'A', 3}};
+
+    public static string[] Pitches(string tonic, string pattern = "mmmmmmmmmmmm")
     {
         var scale = flatKeys.Contains(tonic) ? notesFlat : notes;
-        var startNote = tonic.ToUpperNote();
-        return Enumerable.Range(Array.FindIndex(scale, x => x == startNote), 12)
-            .Select(i => scale[i % 12])
-            .ToArray();
-    }
+        var noteIndex = Array.IndexOf(scale, tonic.First().ToString().ToUpperInvariant() + tonic.Substring(1));
 
-    private static string ToUpperNote(this string note)
-    {
-        var a = note.ToCharArray();
-        a[0] = char.ToUpperInvariant(a[0]);
-        return new string(a);
-    }
-
-    public static string[] Pitches(string tonic, string pattern)
-    {
-        var scale =Pitches( tonic);
-        var test = GetSteps(pattern)
-            .Select(s => scale[s])
-            .ToArray();
-        return test;
-    }
-
-    private static IEnumerable<int> GetSteps( string pattern)
-    {
-        var i = 0;
-        
-        foreach(var p in pattern.Take(pattern.Length))
-        {
-            yield return i;
-            switch(p)
+        return pattern.Select(x =>
             {
-                case 'M':
-                    i += 2;
-                    break;
-                case 'm':
-                    i += 1;
-                    break;
-                case 'A':
-                    i += 3;
-                    break;
-                default:
-                    throw new ArgumentException($"No case for {nameof(p)}:'{p}' in {nameof(pattern)}:'{pattern}'.");
-            }
-        }
+                var i = noteIndex;
+                noteIndex = (noteIndex + Step[x]) % scale.Length;
+                return i;
+            })
+            .Select(x => scale[x])
+            .ToArray();
     }
 }
