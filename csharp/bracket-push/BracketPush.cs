46ccf79f98e4c6202bd7eb2597ed8098a414f7d2
Michael Doolittle <mgdoolittle@gmail.com>
2018-09-09 09:25:59 -0700

    Submitted

--- csharp/bracket-push/BracketPush.cs
+++ csharp/bracket-push/BracketPush.cs
@@ -1,25 +1,24 @@
 using System;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Collections.Generic;
 public static class BracketPush
 {
     public static bool IsPaired(string input)
     {
         var brackets = new Dictionary<char, char>() {{'[', ']'}, {'{', '}'}, {'(', ')'}};
+        var stack = new Stack<char>();
 
-        var test = input.FirstOrDefault(c => brackets.Keys.Contains(c) || brackets.Values.Contains(c));
-
-        if(test != '\0')
+        foreach(var c in input)
         {
-            if(brackets.Values.Contains(test)) return false;
-
-            var pattern = $"[{test}](.*)[{brackets[test]}]";
-            var match = Regex.Match(input, pattern);
-
-            return match.Success && IsPaired(match.Groups[1].Value);
+            if(brackets.ContainsKey(c)) 
+            {
+                stack.Push(brackets[c]);
+            }
+            else if(brackets.ContainsValue(c))
+            {
+                if(stack.Count == 0 || stack.Pop() != c) return false;
+            }
         }
-
-        return true;
+        return stack.Count == 0;
     }
 }
