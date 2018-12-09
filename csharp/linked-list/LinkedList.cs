b0e8017190a1b9ce12cb26334e97ad8976aa6055
Michael Doolittle <mgdoolittle@gmail.com>
2018-12-08 18:42:22 -0800

    Fresh start!

--- csharp/linked-list/LinkedList.cs
+++ csharp/linked-list/LinkedList.cs
@@ -4,38 +4,23 @@ using System.Collections.Generic;
 
 public class Deque<T>
 {
-    private class Node
+    public void Push(T value)
     {
-        public T Value;
-        public Node Next;
-        public Node Previous;
-
-        public Node()
-        {
-            Next = this;
-            Previous = this;
-        }
-
-        internal void Append(T value)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal T Remove()
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
     }
 
-    Node node = new Node();
+    public T Pop()
+    {
+        throw new NotImplementedException();
+    }
 
-    public Deque() => throw new NotImplementedException();
+    public void Unshift(T value)
+    {
+        throw new NotImplementedException();
+    }
 
-    public void Push(T value) => node.Next.Append(value);
-
-    public T Pop() => node.Next.Remove();
-
-    public void Unshift(T value) => throw new NotImplementedException();
-
-    public T Shift() => throw new NotImplementedException();
+    public T Shift()
+    {
+        throw new NotImplementedException();
+    }
 }
