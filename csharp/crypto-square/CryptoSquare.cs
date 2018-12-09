d700cfaf92cca3c48b5da3647f04ed1ee34cf6c6
Michael Doolittle <mgdoolittle@gmail.com>
2018-12-09 13:14:49 -0800

    CryptoSquare - all tests passed

--- csharp/crypto-square/CryptoSquare.cs
+++ csharp/crypto-square/CryptoSquare.cs
@@ -1,25 +1,57 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class CryptoSquare
 {
-    public static string NormalizedPlaintext(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string NormalizedPlaintext(string plaintext) => Regex.Replace(plaintext.ToLowerInvariant(), "\\W", "");
 
     public static IEnumerable<string> PlaintextSegments(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var normalizedText = NormalizedPlaintext(plaintext);
+        var sqrt = Math.Sqrt(normalizedText.Length);
+        var columns = (int)Math.Ceiling(sqrt);
+        var paddedText = normalizedText.PadRight((int)(columns * Math.Round(sqrt)), ' ');
+
+
+        var segments = new List<string>();
+
+        for(var i = 0; i < paddedText.Length; i += columns)
+        {
+            segments.Add(paddedText.Substring(i, columns));
+        }
+
+        return segments;
     }
 
     public static string Encoded(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var segments = PlaintextSegments(plaintext).ToArray();
+        var columns = segments[0].Length;
+        var encoded = string.Empty;
+
+        
+
+        for(var r = 0; r < columns; r++)
+        {
+            for(var c = 0; c < segments.Length; c++)
+            {
+                encoded += segments[c][r];
+            }
+
+            if(r < columns - 1) encoded += ' ';
+        }
+        
+        return encoded;
+
     }
 
     public static string Ciphertext(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var normalizedText = NormalizedPlaintext(plaintext);
+
+        var cipherText = normalizedText.Length > 2 ? Encoded(normalizedText) : normalizedText;
+        return cipherText;
     }
 }
