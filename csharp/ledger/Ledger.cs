5f4fa0d4e01cfab1b4381109aa9dbc9e5a1a256d
Michael Doolittle <mgdoolittle@gmail.com>
2018-12-16 09:47:55 -0800

    got rid of LedgerBuilder class

--- csharp/ledger/Ledger.cs
+++ csharp/ledger/Ledger.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -16,34 +15,42 @@ public class LedgerEntry
     public DateTime Date { get; }
     public string Desc { get; }
     public float Chg { get; }
+
+    public string Formatted(CultureInfo culture)
+    {
+        var date = Date.ToString("d", culture);
+        var description = Desc.Length > 25 ? $"{Desc.Substring(0, 22)}..." : Desc;
+        var change = Chg < 0.0 ? Chg.ToString("C", culture) : Chg.ToString("C", culture) + " ";
+        return $"{date} | {description.PadRight(25)} | {change.PadLeft(13)}";
+    }
 }
 
 public static class Ledger
 {
-    public static string Format(string currency, string locale, LedgerEntry[] entries) =>
-        new LedgerBuilder(currency, locale, entries).Formatted;
-
-    public static LedgerEntry CreateEntry(string date, string desc, int chng) =>
-        new LedgerEntry(DateTime.Parse(date, CultureInfo.InvariantCulture), desc, chng / 100.0f);
-}
-
-internal class LedgerBuilder
-{
-    private CultureInfo _culture;
-    private StringBuilder _formatted;
-
-    public string Formatted => _formatted.ToString();
-
-    public LedgerBuilder(string currency, string locale, LedgerEntry[] entries)
+    public static string Format(string currency, string locale, LedgerEntry[] entries)
     {
-        _culture = CreateCulture(currency, locale);
-        _formatted = new StringBuilder(PrintHead());
+        var culture = CreateCulture(currency, locale);
+        var formatted = new StringBuilder(PrintHead(culture));
         entries
             .OrderBy(x => x.Date)
             .ThenBy(x => x.Desc)
             .ThenBy(x => x.Chg)
             .ToList()
-            .ForEach(x => _formatted.Append("\n" + PrintEntry(x)));
+            .ForEach(x => formatted.Append("\n" + x.Formatted(culture)));
+        return formatted.ToString();
+    }
+
+    private static string PrintHead(CultureInfo culture)
+    {
+        switch (culture.Name)
+        {
+            case "en-US":
+                return "Date       | Description               | Change       ";
+            case "nl-NL":
+                return "Datum      | Omschrijving              | Verandering  ";
+            default:
+                throw new ArgumentException("Invalid locale");
+        }
     }
 
     private static CultureInfo CreateCulture(string cur, string loc)
@@ -79,26 +86,6 @@ internal class LedgerBuilder
         return culture;
     }
 
-    private string PrintHead()
-    {
-        switch (_culture.Name)
-        {
-            case "en-US":
-                return "Date       | Description               | Change       ";
-            case "nl-NL":
-                return "Datum      | Omschrijving              | Verandering  ";
-            default:
-                throw new ArgumentException("Invalid locale");
-        }
-    }
-
-    private string Date(DateTime date) => date.ToString("d", _culture);
-
-    private static string Description(string desc) => desc.Length > 25 ? $"{desc.Substring(0, 22)}..." : desc;
-
-    private string Change(float cgh) => cgh < 0.0 ? cgh.ToString("C", _culture) : cgh.ToString("C", _culture) + " ";
-
-    private string PrintEntry(LedgerEntry entry) =>
-        $"{Date(entry.Date)} | {Description(entry.Desc).PadRight(25)} | {Change(entry.Chg).PadLeft(13)}";
+    public static LedgerEntry CreateEntry(string date, string desc, int chng) =>
+        new LedgerEntry(DateTime.Parse(date, CultureInfo.InvariantCulture), desc, chng / 100.0f);
 }
-
