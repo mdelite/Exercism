c3eaa9691a98d79dca085b29750fba6f7632f47e
Michael Doolittle <mgdoolittle@gmail.com>
2019-05-16 20:10:57 -0700

    all tests passed

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -4,28 +4,14 @@ using System.Linq;
 
 public class HighScores
 {
-    public HighScores(List<int> list)
-    {
-        throw new NotImplementedException();
-    }
+    private List<int> _scores;
+    public HighScores(List<int> list) => _scores = new List<int>(list);
 
-    public List<int> Scores()
-    {
-        throw new NotImplementedException();
-    }
+    public List<int> Scores() => _scores;
 
-    public int Latest()
-    {
-        throw new NotImplementedException();
-    }
+    public int Latest() => _scores.LastOrDefault();
 
-    public int PersonalBest()
-    {
-        throw new NotImplementedException();
-    }
+    public int PersonalBest() => _scores.Max();
 
-    public List<int> PersonalTopThree()
-    {
-        throw new NotImplementedException();
-    }
+    public List<int> PersonalTopThree() => _scores.OrderByDescending(x => x).Take(3).ToList();
 }
