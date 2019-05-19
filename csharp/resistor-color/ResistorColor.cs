51244f8dc46402487886c796f4d1ffe63911d5cc
Michael Doolittle <mgdoolittle@gmail.com>
2019-05-19 09:02:11 -0700

    all tests passed

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ResistorColor
+{
+    private static Dictionary<string, int> colorTable = new Dictionary<string, int>
+    {
+        { "black", 0 },
+        { "brown", 1 },
+        { "red", 2 },
+        { "orange", 3 },
+        { "yellow", 4 },
+        { "green", 5 },
+        { "blue", 6 },
+        { "violet", 7 },
+        { "grey", 8 },
+        { "white", 9 }
+    };
+    public static int ColorCode(string color)
+    {
+        return colorTable[color];
+    }
+
+    public static string[] Colors()
+    {
+        return colorTable.ToList().Select(x => x.Key).ToArray();
+    }
+}
