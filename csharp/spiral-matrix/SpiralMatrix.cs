4c74299d51621ffdc9b764dd7240c5be9ee529b7
Michael Doolittle <mgdoolittle@gmail.com>
2019-05-27 18:13:01 -0700

    second submittal to Exercism

--- csharp/spiral-matrix/SpiralMatrix.cs
+++ csharp/spiral-matrix/SpiralMatrix.cs
@@ -6,44 +6,9 @@ public class SpiralMatrix
 {
     public static int[,] GetMatrix(int size)
     {
+        var rows = GetMatrixRows(size).ToList();
+
         var matrix = new int[size, size];
-
-        var rows = new List<List<int>>();
-
-        int[,] middleMatrix = new int[,] { };
-        if (size >= 3) middleMatrix = GetMatrix(size - 2);
-
-        var offset = (size - 1) * 4; //Offset to apply to middle matrix
-
-        for (int r = 0; r < size; r++)
-        {
-            if (r == 0) //top row
-            {
-                rows.Add(Enumerable.Range(1, size).ToList());
-            }
-            else if (r == size - 1) //bottom row
-            {
-                rows.Add(Enumerable.Range(size * 2 - 1, size).Reverse().ToList());
-            }
-            else  //middle rows
-            {
-                var row = new List<int>();
-                row.Add(offset + 1 - r); //first column
-
-                row.AddRange(
-                    Enumerable.Range(0,middleMatrix.GetLength(1))
-                    .Select(x => middleMatrix[r - 1, x] + offset)
-                );
-
-                row.Add(size + r); //last column
-
-                rows.Add(row);
-            }
-        }
-
-        //fill the matrix
-        
-
         for (var r = 0; r < size; r++)
         {
             for (var c = 0; c < size; c++)
@@ -53,5 +18,37 @@ public class SpiralMatrix
         }
 
         return matrix;
+    }
+
+    private static IEnumerable<List<int>> GetMatrixRows(int size)
+    {
+        var middleMatrix = size > 2 ? GetMatrixRows(size - 2).ToList() : null;
+
+        var offset = (size - 1) * 4; //Offset to apply to middle matrix
+
+        for (int r = 0; r < size; r++)
+        {
+            if (r == 0) //top row
+            {
+                yield return Enumerable.Range(1, size).ToList();
+            }
+            else if (r == size - 1) //bottom row
+            {
+                yield return Enumerable.Range(size * 2 - 1, size).Reverse().ToList();
+            }
+            else  //middle rows
+            {
+                var row = new List<int>();
+
+                row.Add(offset + 1 - r); //first column
+                row.AddRange(
+                    Enumerable.Range(0, middleMatrix[0].Count)
+                    .Select(x => middleMatrix[r - 1][x] + offset)
+                );
+                row.Add(size + r); //last column
+
+                yield return row;
+            }
+        }
     }
 }
