808be4d1dd551a1d99edadd518447ba7e01be2d3
Michael Doolittle <mgdoolittle@gmail.com>
2019-05-30 21:50:55 -0700

    switched from hashset to dictionary

--- csharp/tournament/Tournament.cs
+++ csharp/tournament/Tournament.cs
@@ -3,101 +3,60 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
-
 public static class Tournament
 {
+    public static string resultString => "{0,-30} | {1,2} | {2,2} | {3,2} | {4,2} | {5,2}";
     public static void Tally(Stream inStream, Stream outStream)
     {
-        using (var sw = new StreamWriter(outStream))
+        var teams = new Dictionary<string, TeamResults>();
+
+        using (var sr = new StreamReader(inStream))
         {
-            sw.NewLine = "\n";
-            sw.Write(OutRow("Team", "MP", "W", "D", "L", "P"));
-            
-            var teams = new Teams();
-
-            using (var sr = new StreamReader(inStream))
+            while (!sr.EndOfStream)
             {
-                while (sr.Peek() > -1)
+                var match = sr.ReadLine().Split(';');
+
+                teams.TryAdd(match[0], new TeamResults());
+                teams.TryAdd(match[1], new TeamResults());
+
+                switch (match[2])
                 {
-                    var match = sr.ReadLine().Split(';');
-                    var homeTeam = new Team(match[0]);
-                    var awayTeam = new Team(match[1]);
-                    var result = match[2];
-
-                    switch (result)
-                    {
-                        case "win":
-                            homeTeam.Wins++;
-                            awayTeam.Losses++;
-                            break;
-                        case "loss":
-                            homeTeam.Losses++;
-                            awayTeam.Wins++;
-                            break;
-                        case "draw":
-                            homeTeam.Draws++;
-                            awayTeam.Draws++;
-                            break;
-                        default:
-                            throw new NotImplementedException();
-                    }
-
-                    teams.Add(homeTeam);
-                    teams.Add(awayTeam);
+                    case "win":
+                        teams[match[0]].Wins++;
+                        teams[match[1]].Losses++;
+                        break;
+                    case "loss":
+                        teams[match[0]].Losses++;
+                        teams[match[1]].Wins++;
+                        break;
+                    case "draw":
+                        teams[match[0]].Draws++;
+                        teams[match[1]].Draws++;
+                        break;
                 }
             }
-
-            teams.OrderByDescending(x => x.Points).ThenBy(x => x.Name).ToList().ForEach(x => { 
-                sw.WriteLine();
-                sw.Write(OutRow(x.Name,x.Matches.ToString(),x.Wins.ToString(),x.Draws.ToString(),x.Losses.ToString(),x.Points.ToString()));
-            });
         }
-    }
 
-    public static string OutRow(string team, string matches, string wins, string draws, string losses, string points)
-    {
-        return $"{team,-30} | {matches,2} | {wins,2} | {draws,2} | {losses,2} | {points,2}";
+        using (var sw = new StreamWriter(outStream))
+        {
+            sw.Write(string.Format(resultString, "Team", "MP", "W", "D", "L", "P"));
+            teams.OrderByDescending(x => x.Value.Points).ThenBy(x => x.Key)
+                .ToList()
+                .ForEach(x =>
+                {
+                    sw.Write("\n");
+                    sw.Write(x.Value.Results(x.Key, resultString));
+                });
+        }
     }
 }
 
-public class Team : IEquatable<Team>
+public class TeamResults
 {
-    public Team(string name) => this.Name = name;
-
-    public string Name { set; get; }
     public int Wins { set; get; }
     public int Losses { set; get; }
     public int Draws { set; get; }
-
     public int Matches => Wins + Losses + Draws;
     public int Points => Wins * 3 + Draws;
-
-    public  bool Equals(Team other)
-    {
-        return this.Name.Equals(other.Name);
-    }
-
-    public override int GetHashCode()
-    {
-        return this.Name.GetHashCode();
-    }
-
-    internal void Update(Team team)
-    {
-        this.Wins += team.Wins;
-        this.Losses += team.Losses;
-        this.Draws += team.Draws;
-    }
-}
-
-public class Teams : HashSet<Team>
-{
-    public new void Add(Team team)
-    {
-        var res = base.Add(team);
-        if(!res)
-        {
-            this.Single(x => x.Name == team.Name).Update(team);
-        }
-    }
+    public string Results(string name, string resultsFormat) => string.Format(resultsFormat, name, Matches, Wins, Draws, Losses, Points);
 }
