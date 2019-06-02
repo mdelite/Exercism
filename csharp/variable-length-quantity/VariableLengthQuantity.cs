f1923fcb7551a1371e17e29aee40dec58b75db36
Michael Doolittle <mgdoolittle@gmail.com>
2019-06-02 08:04:36 -0700

    all encode tests passed

--- csharp/variable-length-quantity/VariableLengthQuantity.cs
+++ csharp/variable-length-quantity/VariableLengthQuantity.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class VariableLengthQuantity
 {
     public static uint[] Encode(uint[] numbers)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var chunks = new List<uint>();
+        foreach (var n in numbers)
+        {
+            var stack = new Stack<uint>();
+            var more = 0x00u;
+            var number = n;
+            do
+            {
+                var v = (number & 0x7fu) | more;
+                stack.Push(v);
+                number = number >> 7;
+                more = 0x80u;
+            } while (number > 0);
+            chunks.AddRange(stack);
+        }
+
+        return chunks.ToArray();
     }
 
     public static uint[] Decode(uint[] bytes)
