2cff70548310f8f4f18176c542c7d7affbef6b35
Michael Doolittle <mgdoolittle@gmail.com>
2019-06-04 22:00:02 -0700

    all tests passed

--- csharp/luhn/Luhn.cs
+++ csharp/luhn/Luhn.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Luhn
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var digits = number.Replace(" ", string.Empty);
+        var rx = new Regex(@"^\d{2,}$");
+        if(!rx.IsMatch(digits)) return false;
+
+        return digits.Select(x => (int)char.GetNumericValue(x)).Reverse()
+            .Select((x, i) => (i + 1) % 2 == 0 ? Double(x) : x)
+            .Sum() % 10 == 0;
     }
+
+    private static int Double(int n)
+    {
+         return n * 2 > 9 ? n * 2 - 9 : n* 2;
+    }
+
+     
 }
