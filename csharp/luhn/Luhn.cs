5c87dee9f512f7e13d0a1c7b38039f58a044768e
Michael Doolittle <mgdoolittle@gmail.com>
2019-06-04 22:15:33 -0700

    switched to regex replace

--- csharp/luhn/Luhn.cs
+++ csharp/luhn/Luhn.cs
@@ -7,19 +7,14 @@ public static class Luhn
 {
     public static bool IsValid(string number)
     {
-        var digits = number.Replace(" ", string.Empty);
-        var rx = new Regex(@"^\d{2,}$");
-        if(!rx.IsMatch(digits)) return false;
+        number = Regex.Replace(number,@"\s+", string.Empty);
+        if(!Regex.IsMatch(number, @"^\d{2,}$")) return false;
 
-        return digits.Select(x => (int)char.GetNumericValue(x)).Reverse()
+        return number.Select(x => (int)char.GetNumericValue(x))
+            .Reverse()
             .Select((x, i) => (i + 1) % 2 == 0 ? Double(x) : x)
             .Sum() % 10 == 0;
     }
 
-    private static int Double(int n)
-    {
-         return n * 2 > 9 ? n * 2 - 9 : n* 2;
-    }
-
-     
+    private static int Double(int n) => n * 2 > 9 ? n * 2 - 9 : n * 2;
 }
