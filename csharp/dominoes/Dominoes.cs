86aed7fac81bac8a6399cd0322b3e1865192d519
Michael Doolittle <mgdoolittle@gmail.com>
2019-06-06 05:14:53 -0700

    just starting out

---
+++ csharp/dominoes/Dominoes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Dominoes
+{
+    public static bool CanChain(IEnumerable<(int, int)> dominoes)
+    {
+        return dominoes.Count() == 0;
+    }
+}
