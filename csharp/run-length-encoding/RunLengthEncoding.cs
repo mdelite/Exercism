d823c798919b074a3358ad78604e84f3e2fd6cec
Michael Doolittle <mgdoolittle@gmail.com>
2019-10-13 13:35:00 -0700

    switched to regex replace

--- csharp/run-length-encoding/RunLengthEncoding.cs
+++ csharp/run-length-encoding/RunLengthEncoding.cs
@@ -1,39 +1,20 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class RunLengthEncoding
 {
     public static string Encode(string input)
     {
         var regex = new Regex(@"(.)\1*");
-        var output = "";
 
-        foreach (Match match in regex.Matches(input))
-        {
-            if(match.Value.Length > 1)
-            {
-                output += match.Value.Length.ToString();
-            }
-
-            output += match.Value.Substring(0,1);
-        }
-
-        return output;
+        return regex.Replace(input, m => (m.Value.Length > 1 ? m.Value.Length.ToString() : "") + m.Value[0].ToString());
     }
 
     public static string Decode(string input)
     {
         var regex = new Regex(@"(\d*)(.)");
-        var output = "";
 
-        foreach (Match match in regex.Matches(input))
-        {
-            var c = 0;
-            if ( !int.TryParse(match.Groups[1].Value, out c)) c = 1;
-
-            output += new string(Char.Parse( match.Groups[2].Value),c);
-        }
-
-        return output;
+        return regex.Replace(input, m => new string(m.Groups[2].Value[0], m.Groups[1].Value == "" ? 1 : int.Parse(m.Groups[1].Value)));
     }
 }
