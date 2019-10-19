c32193fe833e328e70f02006eb3aa3faaa772567
Michael Doolittle <mgdoolittle@gmail.com>
2019-10-19 07:35:58 -0700

    all left to right tests passed

--- csharp/word-search/WordSearch.cs
+++ csharp/word-search/WordSearch.cs
@@ -4,13 +4,26 @@ using System.Linq;
 
 public class WordSearch
 {
+    private string[] _rows;
     public WordSearch(string grid)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _rows = grid.Split("\n");
     }
 
     public Dictionary<string, ((int, int), (int, int))?> Search(string[] wordsToSearchFor)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return  wordsToSearchFor.ToDictionary( x => x, y => GetCoords(y));
+    }
+    private ((int, int), (int, int))? GetCoords( string word)
+    {
+        for(var r = 0; r < _rows.Length; r++)
+        {
+            var found = _rows[r].IndexOf(word);
+            if(found >= 0)
+            {
+                return ((found + 1, r+ 1), (found + word.Length, r+1));
+            }
+        }
+        return null;
     }
 }
