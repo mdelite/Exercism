0faa10f5677b6c4b34df176b254b62941d64b8e6
Michael Doolittle <mgdoolittle@gmail.com>
2019-12-01 07:33:01 -0800

    picked up spares

--- csharp/bowling/Bowling.cs
+++ csharp/bowling/Bowling.cs
@@ -3,18 +3,48 @@
 public class BowlingGame
 {
     int _score;
+    int _bonus;
+    bool _newFrame;
+    int _frameScore;
 
     public BowlingGame()
     {
         _score = 0;
+        _bonus = 0;
+        _newFrame = true;
+        _frameScore = 0;
     }
     public void Roll(int pins) 
     {
         _score += pins;
+        _frameScore += pins;
+
+        if(_bonus > 0)
+        {
+            _score += pins;
+            _bonus--;
+        }
+
+        if(_frameScore == 10)
+        {
+            _bonus = 1;
+        }
+
+        if(!_newFrame)
+        {
+            _frameScore = 0;
+        }
+
+        _newFrame = !_newFrame;
     }
 
     public int? Score()
     {
         return _score;
+    }
+
+    private void ResetFrame()
+    {
+        _newFrame = true;
     }
 }
