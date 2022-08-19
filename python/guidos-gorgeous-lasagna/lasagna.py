039837cba9cb73f9a1a5cd0c3ce047cd2bb1e57c
Michael Doolittle <mgdoolittle@gmail.com>
2022-08-18 20:09:04 -0700

    progress

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -2,11 +2,11 @@
 
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
-
 # TODO: define the 'EXPECTED_BAKE_TIME' constant
+EXPECTED_BAKE_TIME = 40
 # TODO: consider defining the 'PREPARATION_TIME' constant
 #       equal to the time it takes to prepare a single layer
-
+PREPARATION_TIME = 0
 
 # TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining():
@@ -25,6 +25,9 @@ def bake_time_remaining():
 
 # TODO: define the 'preparation_time_in_minutes()' function
 #       and consider using 'PREPARATION_TIME' here
-
+def preparation_time_in_minutes():
+    pass
 
 # TODO: define the 'elapsed_time_in_minutes()' function
+def elapsed_time_in_minutes():
+    pass
