b96e251b61331d29d49a49a1d40f57219c2c7c19
Michael Doolittle <mgdoolittle@gmail.com>
2022-08-19 20:41:06 -0700

    all tests passed

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -2,14 +2,11 @@
 
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-EXPECTED_BAKE_TIME = 40
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
-PREPARATION_TIME = 0
 
-# TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -19,15 +16,28 @@ def bake_time_remaining():
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-    pass
+def preparation_time_in_minutes(layers):
+    """Calculate the preperation time
 
+    :param layers: int - the number of layers to prepare
+    :return: int - time (in minutes) to prepare the lasagna
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
-def preparation_time_in_minutes():
-    pass
+    Function that the number of layers and returns how many minutes to prepare
+    based on the 'PREPARATION_TIME'.
+    """
+    return layers * PREPARATION_TIME
 
-# TODO: define the 'elapsed_time_in_minutes()' function
-def elapsed_time_in_minutes():
-    pass
+def elapsed_time_in_minutes(layers, time):
+    """Calculate the time elapsed.
+
+    :param layers: int - the number of layers in hte lasagna. 
+    :param time: int 
+    :return: int - time (in minutes) that has elapsed.
+
+    Function that calculates the total time that has elapsed in the preperation of
+    the lasagna. 
+    """
+    elapsed = preparation_time_in_minutes(layers) + time
+    return elapsed
