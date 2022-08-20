08f0449e1158a5e9e4a82226081b9e1b9d9151a7
Michael Doolittle <mgdoolittle@gmail.com>
2022-08-19 20:56:02 -0700

    removed trailing spaces

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -32,12 +32,12 @@ def preparation_time_in_minutes(layers):
 def elapsed_time_in_minutes(layers, time):
     """Calculate the time elapsed.
 
-    :param layers: int - the number of layers in hte lasagna. 
-    :param time: int 
+    :param layers: int - the number of layers in hte lasagna.
+    :param time: int
     :return: int - time (in minutes) that has elapsed.
 
     Function that calculates the total time that has elapsed in the preperation of
-    the lasagna. 
+    the lasagna.
     """
     elapsed = preparation_time_in_minutes(layers) + time
     return elapsed
