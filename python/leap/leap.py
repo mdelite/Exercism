7d41ca67a8fff3f8f015fdfc7a5c382c1148c6ec
Michael Doolittle <mgdoolittle@gmail.com>
2022-08-22 19:20:08 -0700

    taken down to single line

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,14 +1,6 @@
 """Module with leap year functions"""
 
 def leap_year(year):
-    """Determine if year is a leap year
-
-    param year: int - the year to test
-    """
-
-    if year % 4 > 0:
-        return False
-    if year % 100 == 0 and year % 400 != 0:
-        return False
-    return True
-
+    """Determine if year is a leap year"""
+    
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
