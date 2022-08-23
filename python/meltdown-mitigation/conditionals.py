5fee77ed5a5cfeee83d6a826bcc99d766886301c
Michael Doolittle <mgdoolittle@gmail.com>
2022-08-22 21:20:28 -0700

    all tests passed

--- python/meltdown-mitigation/conditionals.py
+++ python/meltdown-mitigation/conditionals.py
@@ -14,7 +14,7 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
 
-    pass
+    return temperature < 800 and neutrons_emitted > 500 and temperature * neutrons_emitted < 500000
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
@@ -37,7 +37,17 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
     where generated power = voltage * current
     """
 
-    pass
+    power = voltage * current
+    efficiency = power / theoretical_max_power
+    
+    if efficiency >= 0.80:
+        return "green"
+    elif efficiency >= 0.60:
+        return "orange"
+    elif efficiency >= 0.30:
+        return "red"
+    else:
+        return "black"
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
@@ -53,4 +63,12 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
     """
 
-    pass
+    val = temperature * neutrons_produced_per_second / threshold
+
+    if val < 0.90:
+        return "LOW"
+    elif val <=1.10:
+        return "NORMAL"
+    else:
+        return "DANGER"
+        
