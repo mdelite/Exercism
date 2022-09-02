df336e83b088d6ab873e4105a6aa52a788cbb6ff
Michael Doolittle <mgdoolittle@gmail.com>
2022-09-01 18:38:33 -0700

    all tests passed

--- python/chaitanas-colossal-coaster/list_methods.py
+++ python/chaitanas-colossal-coaster/list_methods.py
@@ -11,7 +11,11 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     :return: list - the (updated) queue the name was added to.
     """
 
-    pass
+    if ticket_type == 1:
+        express_queue.append(person_name)
+        return express_queue
+    normal_queue.append(person_name)
+    return normal_queue
 
 
 def find_my_friend(queue, friend_name):
@@ -22,6 +26,9 @@ def find_my_friend(queue, friend_name):
     :return: int - index at which the friends name was found.
     """
 
+    for index, value in enumerate(queue):
+        if value == friend_name:
+            return index
     pass
 
 
@@ -34,7 +41,8 @@ def add_me_with_my_friends(queue, index, person_name):
     :return: list - queue updated with new name.
     """
 
-    pass
+    queue.insert(index, person_name)
+    return queue
 
 
 def remove_the_mean_person(queue, person_name):
@@ -45,7 +53,8 @@ def remove_the_mean_person(queue, person_name):
     :return: list - queue update with the mean persons name removed.
     """
 
-    pass
+    queue.remove(person_name)
+    return queue
 
 
 def how_many_namefellows(queue, person_name):
@@ -56,7 +65,7 @@ def how_many_namefellows(queue, person_name):
     :return: int - the number of times the name appears in the queue.
     """
 
-    pass
+    return queue.count(person_name)
 
 
 def remove_the_last_person(queue):
@@ -66,7 +75,7 @@ def remove_the_last_person(queue):
     :return: str - name that has been removed from the end of the queue.
     """
 
-    pass
+    return queue.pop(-1)
 
 
 def sorted_names(queue):
@@ -76,4 +85,5 @@ def sorted_names(queue):
     :return: list - copy of the queue in alphabetical order.
     """
 
-    pass
+    queue.sort()
+    return queue
