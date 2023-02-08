6f379dff8edf52c930c58ba7cd388130b3a38f96
Michael Doolittle <mgdoolittle@gmail.com>
2023-02-07 19:35:03 -0800

    all tests passed

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,2 +1,6 @@
 def transform(legacy_data):
-    pass
+    scores = {}
+    for val, letters in legacy_data.items():
+        for letter in letters:
+            scores[letter.lower()] = val
+    return scores
