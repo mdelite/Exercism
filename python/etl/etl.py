8fe77a03ff6478d0d89e955a0daf137450ab59bc
Michael Doolittle <mgdoolittle@gmail.com>
2023-02-07 19:48:42 -0800

    distilled down to one line

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,6 +1,2 @@
 def transform(legacy_data):
-    scores = {}
-    for val, letters in legacy_data.items():
-        for letter in letters:
-            scores[letter.lower()] = val
-    return scores
+    return {letter.lower(): val for val in legacy_data for letter in legacy_data[val]}
